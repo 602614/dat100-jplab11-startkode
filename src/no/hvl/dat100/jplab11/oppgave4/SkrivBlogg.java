cac3d267dae09cfca96c1cbd114077ef6d8652a2
sindr <sindr@DESKTOP-S15KEAJ>
2021-11-19 23:53:35 +0100

    ferdig

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,15 +1,35 @@
 package no.hvl.dat100.jplab11.oppgave4;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
 
-public class SkrivBlogg {
+public class SkrivBlogg extends Blogg {
 
-	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+    public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
-	}
+        String melding = "";
+
+    try {
+        String fil1 = ("Blogg.txt");
+        File fil = new File(fil1);
+        PrintWriter skriver = new PrintWriter (fil1);
+
+        Blogg[] tabell = new Blogg(nesteledige);
+
+        for (int i = 0; i < tabell.lentgh; i++) {
+            skriver.println();
+
+            skriver.close();
+        }
+    }
+
+        catch (FileNotFoundException e) {
+            melding = "filen finnes ikke";
+        }
+
+    }
 }
